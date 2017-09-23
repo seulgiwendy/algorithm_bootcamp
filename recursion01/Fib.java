8b9ae9c687144d2867e0220263e8713211bcc354
wheejuni <wheejuni@gmail.com>
2017-09-23 21:17:12 +0900

    피보나치 수열 알고리즘 구현 (재귀함수)

---
+++ recursion01/Fib.java
@@ -0,0 +1,22 @@
+public class Fib{
+
+	public static void main (String args[]){
+		
+		int n = 5;
+		System.out.println(fib(n));
+	}
+
+	public static int fib(int n){
+		
+		if (n <= 1){
+			return n;
+		}
+
+		else{
+
+			return fib(n - 1) + fib(n - 2);
+		}
+
+	}
+}
+
