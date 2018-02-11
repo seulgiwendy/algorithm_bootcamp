b424bb8dcfd0cd96d5dff7de0bc7ddc093d3304d
wheejuni <wheejuni@gmail.com>
2018-02-11 22:57:15 +0900

    [BFS]무한루프 방지 로직 구현

--- search/bfs/bfs.py
+++ search/bfs/bfs.py
@@ -3,7 +3,7 @@ from collections import deque
 map = {}
 
 map["me"] = ["honux", "pobi", "jk"]
-map["honux"] =["dayoungle-java", "dongjun"]
+map["honux"] =["dayoungle", "dongjun"]
 map["pobi"] = ["tobi", "jojoldu-java"]
 map["jk"] = ["jobs"]
 
@@ -11,15 +11,24 @@ def search_for_java_dev(map):
     search_queue = deque()
 
     search_queue += map
+    searched = []
 
     while search_queue:
         person = search_queue.popleft()
-        print("currently indexing " + person)
+        print("currently searching " + person)
+        if person in searched:
+            print("we've already inspected you, so we'll pass to next iteration.")
+            continue
+
         if is_java_developer(person):
             print(person + " is a java dev!")
             return True
         else:
-            search_queue += map[person]
+            print("no! you don't speak java.")
+            searched.append(person)
+            if person in map:
+                search_queue += map[person]
+                print(search_queue)
 
 def is_java_developer(name):
     return "java" in name
