0aee2f5128521834f5ab6d375baeb1f9220c480b
wheejuni <wheejuni@gmail.com>
2018-02-12 20:32:40 +0900

    [greedy]퀵소트로 강의리스트 정렬 후 처리

--- greedy/timetable.py
+++ greedy/timetable.py
@@ -1,4 +1,6 @@
+import random
 from collections import deque
+from datetime import datetime
 
 
 class Classtime:
@@ -17,6 +19,9 @@ class Classtime:
     def __repr__(self):
         return "%s : %d시 ~ %d시" % (self.subject, self.start_time, self.end_time)
 
+    def __lt__(self, other):
+        return self.start_time < other.start_time
+
 class_list = []
 
 
@@ -24,6 +29,7 @@ class1 = Classtime(9, 11, "알고리즘")
 class2 = Classtime(10, 12, "웹프로그래밍")
 class3 = Classtime(8, 9, "정신교육")
 class4 = Classtime(13, 14, "React.js 특강")
+class5 = Classtime(11, 13, "Clean code by TDD with Java")
 
 print(class1.is_conflict(class2))
 
@@ -31,6 +37,8 @@ class_list.append(class1)
 class_list.append(class2)
 class_list.append(class3)
 class_list.append(class4)
+class_list.append(class5)
+
 
 def arrange_schedule(sessions_list):
 
@@ -62,10 +70,34 @@ def first_class(sessions_list):
 
     return earliest_class
 
-print(first_class(class_list))
-print(arrange_schedule(class_list))
+def sort_classes(class_list):
+
+    if len(class_list) < 2:
+        return class_list
+
+    else:
+        random_index = random.randrange(0, len(class_list))
+
+        pivot = class_list[random_index]
+
+        smaller = []
+        greater = []
+
+        for i in range(0, len(class_list)):
+            if i == random_index:
+                continue
+
+            if class_list[i].start_time <= pivot.start_time:
+                smaller.append(class_list[i])
+
+            else:
+                greater.append(class_list[i])
+
+    return sort_classes(smaller) + sort_classes([pivot]) + sort_classes(greater)
 
 
+sorted_class_list = sort_classes(class_list)
+print(arrange_schedule(sorted_class_list))
 
 
 
