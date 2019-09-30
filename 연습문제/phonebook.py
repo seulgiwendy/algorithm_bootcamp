58b47f54572f888b97b3bfe1182b9354777abd8e
wheejuni <wheejuni@gmail.com>
2019-09-30 11:30:33 +0900

    전화번호 목록 풀이완료

--- 연습문제/phonebook.py
+++ 연습문제/phonebook.py
@@ -1,23 +1,34 @@
 def solution(phone_book):
-    answer = True
-
     prefix_hash = {}
+    listified_numbers = []
 
     for number in phone_book:
         if number in prefix_hash:
             return False
         prefix_hash[number] = 1
+        listified_numbers.append(listify_number(number))
 
-    for prefix in prefix_hash:
-        pass
+    for numbers_list in listified_numbers:
+        for number in numbers_list:
+            if number in prefix_hash:
+                prefix_hash[number] += 1
 
+    for prefixes in prefix_hash:
+        if prefix_hash[prefixes] > 2:
+            return False
 
-    return answer
+    return True
+
 
 def listify_number(number):
+    listified_number = []
+    splitted_number = list(number)
 
-    for i in range(len(number)):
-        pass
-    pass
+    for j in range(1, len(splitted_number) + 1):
+        listified_number.append(''.join(splitted_number[0:j]))
 
-print solution(["12", "123", "1235", "567", "88"])
+    return listified_number
+
+
+print(solution(["12", "123", "1235", "567", "88"]))
+print(solution(["123", "456", "789"]))
