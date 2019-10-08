c0f923a84f9462fbce57351383438220d99fa5e1
wheejuni <wheejuni@gmail.com>
2019-10-08 19:05:17 +0900

    퍼뮤테이션 기반 풀이 완료

--- 연습문제/biggest_generatable_number.py
+++ 연습문제/biggest_generatable_number.py
@@ -1,24 +1,27 @@
 from itertools import permutations
 
-def solution(numbers):
-    generated_numbers = []
 
-    permutations = generate_possible_permutations(numbers)
+def solution(numbers):
+    generated_numbers = generate_possible_permutations(numbers)
+    joined_numbers = []
+
+    for number in generated_numbers:
+        fetched_numbers = ""
+        for lower_index in range(0, len(numbers)):
+            fetched_numbers += str(number[lower_index])
+
+        joined_numbers.append(int(fetched_numbers))
+
+    joined_numbers.sort()
+
+    return str(joined_numbers[-1]).replace(".0", "")
 
 
 def generate_possible_permutations(numbers):
-    generated_permutations = []
-
-    permutation_target = []
-
-    for i in range(0, len(numbers)):
-        permutation_target.append(i)
-
-    return permutations(permutation_target, len(numbers))
+    return list(permutations(numbers, len(numbers)))
 
 
-for permu in generate_possible_permutations([1, 2, 3, 4]):
-    print(permu)
+print(solution([3, 30, 34, 5, 9]))
 
 
 
