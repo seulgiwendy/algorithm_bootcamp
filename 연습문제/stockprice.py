013de1280542f3a4c273ef5a3950deab02af0e6a
wheejuni <wheejuni@gmail.com>
2020-10-04 20:54:09 +0900

    정확도 만점, 효율성 빵점

--- 연습문제/stockprice.py
+++ 연습문제/stockprice.py
@@ -2,19 +2,22 @@ def solution(prices):
     answer = []
 
     for i in range(len(prices)):
+        remaining_prices = prices[i + 1:]
+        time_count = 0
 
-        for j in range(i + 1, len(prices)):
+        if i == len(prices) - 1:
+            answer.append(time_count)
+            return answer
 
-            print("i price: " + str(prices[i]))
-            print("j price: " + str(prices[j]))
+        for j in range(len(remaining_prices)):
+            time_count += 1
 
-            if prices[i] > prices[j]:
-                answer.append(j - i)
+            if prices[i] > remaining_prices[j]:
+                answer.append(time_count)
+                break
 
-            if j == len(prices) - 1:
-                answer.append(j - i)
+            if j == len(remaining_prices) - 1:
+                answer.append(time_count)
+                break
 
     return answer
-
-
-print(solution([1, 2, 3, 2, 3]))
